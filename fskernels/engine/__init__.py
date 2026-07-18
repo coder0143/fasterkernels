bdc7efccfee562e15fd86312eecff56d08fc0e47
Pratham Shah <shahpratham688@gmail.com>
2026-07-18 17:52:15 +0530

    fix

--- fskernels/engine/__init__.py
+++ fskernels/engine/__init__.py
@@ -1,11 +1,9 @@
 from fskernels.engine.cuda_graph_runner import CUDAGraphCache, CUDAGraphRunner
 from fskernels.engine.fs_inference_engine import FsInferenceEngine, sample_token
-from fskernels.engine.speculative_engine import SpeculativeEngine
 
 __all__ = [
     "CUDAGraphCache",
     "CUDAGraphRunner",
     "FsInferenceEngine",
-    "SpeculativeEngine",
     "sample_token",
 ]
